a7ada03197eaf545d62eb246f837e8c9c02e2c97
ahuerta6 <ahuerta2@hawk.iit.edu>
2022-11-05 14:41:00 -0500

    adding flavor

--- js/results.js
+++ js/results.js
@@ -1,6 +1,9 @@
 function required() {
  if(document.getElementById('first').value == "" || document.getElementById('last').value == "" || document.getElementById('input-email').value == ""){
   alert('Please enter your information');
+  document.getElementById('first'.style.borderColor = "yellow");
+  document.getElementById('last'.style.borderColor = "yellow");
+  document.getElementById('input-email'.style.borderColor = "yellow");
   return false;
  }
 }
