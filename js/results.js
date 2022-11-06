b0aa583c754d1bed15e2bfebe8ce9e3378d2557c
ahuerta6 <ahuerta2@hawk.iit.edu>
2022-11-06 12:41:40 -0600

    using &&
    will use if statement if all inputs are not filled not if only one is not filled

--- js/results.js
+++ js/results.js
@@ -1,5 +1,5 @@
 function required() {
- if(document.getElementById('first').value == "" || document.getElementById('last').value == "" || document.getElementById('input-email').value == ""){
+ if(document.getElementById('first').value == "" && document.getElementById('last').value == "" && document.getElementById('input-email').value == ""){
   alert('Please enter your information');
   document.getElementById('first'.style.borderColor = "yellow");
   document.getElementById('last'.style.borderColor = "yellow");
